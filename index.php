a4b8b26d280ec76cbc62186949413d93a99a6505
omar-diane <omar.diane@laplateforme.io>
2022-02-16 12:01:16 +0100

    Erreur undefined variable word

--- index.php
+++ index.php
@@ -1,7 +1,41 @@
 <?php
 session_start();
 
-$str = file_get_contents('mots.txt');
-$result = explode('', $str);
-$result = $result [array_rand($result)];
-$_SESSION["word"] = $result;
+$string = file_get_contents('mots.txt');
+$result = explode(' ',$string);
+$result = $result[array_rand($result)];
+$_GET['word'] = $result;
+
+if(isset($_GET['word'])){
+    $user_word = $_GET['word'];
+
+    if($user_word < $word){
+    echo 'Mot trop petit.';
+    } elseif 
+        ($user_word > $word) {
+            echo 'Mot trop long.';
+        } elseif
+            ($user_word === $word){
+                echo 'Bravo !';
+            }
+        }
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Jeu du pendu</title>
+</head>
+<body>
+    <main>
+        <form action="" method="get">
+            <input type="text" name="word" />
+            <input type="submit" value="Envoyer">
+        </form>
+    </main>
+    
+</body>
+</html>
