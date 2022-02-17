d23a81064c6f59cc708e3848699bf4a9a2760e71
omar-diane <omar.diane@laplateforme.io>
2022-02-17 14:52:54 +0100

    Commit avec des lignes de code pour choisir le mot aléatoirement dans le fichier mots.txt.

--- index.php
+++ index.php
@@ -1,40 +1,75 @@
 <?php
-session_start();
 
-$letter;
-$word = file_get_contents('mots.txt');
+//Démarrer une session pour faire appel au variables de session et sauver les infos nécessaires pour le fonctionnement du jeu
+    session_start();
 
-if(isset($_GET['word'])){
-    //Je récupère le mot qui est envoyer.
-    $user_word = $_GET['word'];
-    /*Je test si l'utilisateur rentre un mot et que le mot est différent du mot
-      qui doit être trouvé alors il echo un message*/ 
-    if($user_word != $word) {
-        echo 'Pas le bon mot';
-        /*Si l'utilisateur rentre un mot et que le mot est le mot à trouvé,
-          il echo un message*/
-        elseif ($user_word === $word)
-        echo 'BRAVO !';
-    } else 
-    echo 'Essayez';
+    $str = array (file_get_contents('mots.txt'));
+    $rand_keys = array_rand($str);
+
+    //Créer des variables de session
+    $_SESSION['wordAffiche'] = array();
+    $_SESSION['lettresJouees'] = array();
+    $_SESSION['word'] = "";
+    $_SESSION['nbTentatives'] = 0;
+    $_SESSION['longueur'] = 0;
+    $_SESSION['nblettrestrouvees'] = 0;
+
+    //Enregistrer le mot à découvrir dans une variable session
+    $_SESSION['word'] = $_POST['word'];
+
+    //Sauvegarder la longueur du mot
+    $_SESSION['longueur'] = strlen($_SESSION['word']);
+
+    //Remplir les tableaux (initialisation)
+    for($i = 1 ; $i <= $_SESSION['longueur'] ; $i++){
+
+        //Mettre des _ dans le mot a afficher
+        $_SESSION['wordAffiche'][] = "_";
+
+    }
+
+    //Initialiser tout le tableau lettresJouees à FAUX
+    for($i = 1 ; $i <= 26 ; $i++){
+
+        $_SESSION['lettresJouees'][] = false;
+
+    }
 
 ?>
 
 <!DOCTYPE html>
-<html lang="fr">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Jeu du pendu</title>
+    <title>Document</title>
 </head>
 <body>
     <main>
-        <form action="" method="get">
-            <input type="text" name="word" />
-            <input type="submit" value="Envoyer">
-        </form>
+        <div>
+
+        <?php
+
+        //Affiche le mot avec des - ou les lettres trouvees
+foreach($_SESSION['wordAffiche'] as $rang => $element){
+
+    //Afficher le mot a afficher avec ou sans les - ou les lettres trouvees
+    echo $element;
+}
+
+//Aller à la ligne
+echo "<BR> ";
+
+//Tant que i < 26 (avec initialisation de i à 0 et incrementation a 1) faire...
+for($i = 0 ; $i < 26 ; $i++){
+
+    //Afficher la lettre actuelle sans son lien
+    echo " <a href=\"index2.php?lettre=$i\">", chr(65 + $i), "</a> ";
+}
+
+?>
+        </div>
     </main>
-    
 </body>
 </html>
